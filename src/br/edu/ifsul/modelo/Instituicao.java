16ce8af1e0f894787529918e30155310acaf938c
Joel <Joel@user>
2016-08-22 00:28:56 -0300

    Primeiro Commit do Projeto

---
+++ src/br/edu/ifsul/modelo/Instituicao.java
@@ -0,0 +1,73 @@
+package br.edu.ifsul.modelo;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Joel Zatti joelzatti@gmail.com
+ */
+@Entity
+public class Instituicao implements Serializable {
+
+    @Id
+    private Integer id;
+    private String nome;
+    private Calendar anoFundacao;
+
+    public Instituicao() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Calendar getAnoFundacao() {
+        return anoFundacao;
+    }
+
+    public void setAnoFundacao(Calendar anoFundacao) {
+        this.anoFundacao = anoFundacao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Instituicao other = (Instituicao) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+}
